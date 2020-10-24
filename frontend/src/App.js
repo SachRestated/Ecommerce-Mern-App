7202806351200c748bad152293acca5478c613ff
SachRestated <sachinvashisht1999@gmail.com>
2020-10-24 18:48:54 +0530

    Setup react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
 function App() {
   return (
-    <div>
-      <h1>Welcome to Mern Shop</h1>
-    </div>
+    <>
+      <Header />
+        <main className="py-3">
+          <Container>
+            <h1>Welcome to Mern Shop</h1>
+          </Container>
+        </main>
+      <Footer />
+    </>
   );
 }
 
