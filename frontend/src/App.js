c96d62a051831aa23c4137f9fc7564307122e29d
SachRestated <sachinvashisht1999@gmail.com>
2020-10-24 23:38:36 +0530

    Added Rating Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
-
+import HomeScreen from './screens/HomeScreen'
+ 
 function App() {
   return (
     <>
       <Header />
         <main className="py-3">
           <Container>
-            <h1>Welcome to Mern Shop</h1>
+            <HomeScreen />
           </Container>
         </main>
       <Footer />
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Rating = ({value, text, color}) => {
+    const stars = []
+    while(value > 0) {
+        if(value >= 1) {
+            stars.push(
+                <span>
+                    <i style={{color}}className='fas fa-star'></i>
+                </span>
+            )
+        } 
+        else {
+            stars.push(
+                <span style={{color}}>
+                    <i className='fas fa-star-half-alt'></i>
+                </span>
+            )
+        }
+        value -= 1;
+    }
+
+    while(stars.length !== 5) {
+        stars.push(
+            <span style={{color}}>
+                <i className='far fa-star'></i>
+            </span>
+        )
+    }
+    stars.push(<span>{text && ` ${text}`}</span>)
+
+    return (
+        <div className='rating'>
+            {stars.map((star) => star)}
+        </div>
+    )
+}
+
+Rating.defaultProps = {
+    color: '#f8d825'
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string
+}
+
+
+export default Rating
