422bd6b83219b63d31ebdb0462c9fe035299f7a0
SachRestated <sachinvashisht1999@gmail.com>
2020-10-26 12:51:06 +0530

    Error Handling and Loading symbol

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product' 
+import Loader from '../components/Loader' 
+import Message from '../components/Message' 
+
 import { listProduct } from '../actions/productActions'
 
 
@@ -22,9 +25,9 @@ const HomeScreen = () => {
             <h1>Latest Products</h1>
             {
                 loading ? 
-                    <h2>Loading....</h2> : 
+                    <Loader /> : 
                 error ? 
-                    <h3>{error} </h3> : 
+                    <Message variant={'danger'} children={error}/> : 
                     <Row>
                         {products.map( product => {
                             return (
